caba245745b023061fb4be506249083ac2f7fe23
Anthony Dimas Solano Torres <93417377+anthonyp20@users.noreply.github.com>
2022-08-29 10:26:55 -0500

    Add files via upload

---
+++ Tabla Producto/Producto.AppWin/frmClienteEdit.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Financiera.AppWin
+{
+    partial class frmClienteEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.chkEstado = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombres:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(131, 32);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(279, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Marca";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(496, 32);
+            this.txtMarca.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(320, 22);
+            this.txtMarca.TabIndex = 3;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrabar.Location = new System.Drawing.Point(716, 156);
+            this.btnGrabar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(100, 28);
+            this.btnGrabar.TabIndex = 12;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.grabarDatos);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Location = new System.Drawing.Point(496, 156);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(100, 28);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // chkEstado
+            // 
+            this.chkEstado.AutoSize = true;
+            this.chkEstado.Location = new System.Drawing.Point(13, 156);
+            this.chkEstado.Margin = new System.Windows.Forms.Padding(4);
+            this.chkEstado.Name = "chkEstado";
+            this.chkEstado.Size = new System.Drawing.Size(304, 21);
+            this.chkEstado.TabIndex = 16;
+            this.chkEstado.Text = "¿Cliente disponible para proceso de datos?";
+            this.chkEstado.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Precio:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(419, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Stock:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(131, 97);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(279, 22);
+            this.txtPrecio.TabIndex = 19;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(496, 97);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(320, 22);
+            this.txtStock.TabIndex = 20;
+            // 
+            // frmClienteEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 220);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkEstado);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmClienteEdit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Cliente";
+            this.Load += new System.EventHandler(this.iniciarFormulario);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.CheckBox chkEstado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtStock;
+    }
+}
---
+++ Tabla Producto/Producto.Data/ClienteData.cs
@@ -0,0 +1,136 @@
+﻿using Financiera.Dominio;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Financiera.Data
+{
+    public class ClienteData
+    {
+        string cadenaConexion = "server=localhost; Database=Financiera; Integrated Security = true";
+        public List<Cliente> Listar()
+        {
+            var listado = new List<Cliente>();
+            using(var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using(var comando = new SqlCommand("Select * From Cliente", conexion))
+                {
+                    using(var lector = comando.ExecuteReader())
+                    {
+                        if(lector != null && lector.HasRows)
+                        {
+                            Cliente cliente;
+                            while (lector.Read())
+                            {
+                                cliente = new Cliente();
+                                cliente.ID = int.Parse(lector[0].ToString());
+                                cliente.Nombres = lector[1].ToString();
+                                cliente.Marca = lector[2].ToString();
+                                cliente.Precio = lector[3].ToString();
+                                cliente.Stock = lector[4].ToString();
+                                
+
+                                listado.Add(cliente);
+                            }
+                        }
+                    }
+                }
+            }
+            return listado;
+        }
+
+        public Cliente BuscarPorId(int id)
+        {
+            Cliente cliente = new Cliente();
+            using(var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using(var comando = new SqlCommand("SELECT * FROM Cliente WHERE ID = @id",conexion))
+                {
+                    comando.Parameters.AddWithValue("@id", id);
+                    using(var lector = comando.ExecuteReader())
+                    {
+                        if(lector!= null && lector.HasRows)
+                        {
+                            lector.Read();
+                            cliente = new Cliente();
+                            cliente.ID = int.Parse(lector[0].ToString());
+                            cliente.Nombres = lector[1].ToString();
+                            cliente.Marca = lector[2].ToString();
+                            cliente.Precio = lector[3].ToString();
+                            cliente.Stock = lector[4].ToString();
+                            
+                        }
+                    }
+                }
+            }
+            return cliente;
+        }
+
+        public bool Insertar(Cliente cliente)
+        {
+            int filasInsertadas = 0;
+            using(var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                var sql = "INSERT INTO Cliente (Nombres,Marca, " +
+                                "Direccion, Referencia, IdTipoCliente, IdTipoDocumento, " +
+                                "NumeroDocumento, Estado)" +
+                            "VALUES(@Nombres, @Apellidos, @Direccion, @Referencia, " +
+                                "@IdTipoCliente, @IdTipoDocumento, @NumeroDocumento, @Estado)";
+                using(var comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.AddWithValue("@Nombres", cliente.Nombres);
+                    comando.Parameters.AddWithValue("@Marca", cliente.Marca);
+                    comando.Parameters.AddWithValue("@Precio", cliente.Precio);
+                    comando.Parameters.AddWithValue("@Stock", cliente.Stock);
+                    comando.Parameters.AddWithValue("@Estado", cliente.Estado);
+                    comando.Parameters.AddWithValue("@ID", cliente.ID);
+                    filasInsertadas = comando.ExecuteNonQuery();
+                }
+            }
+            return filasInsertadas > 0;
+        }
+
+        public bool Actualizar(Cliente cliente)
+        {
+            int filasActualizadas = 0;
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                var sql = "UPDATE Cliente SET Nombres = @Nombres, Apellidos = @Apellidos, " +
+                    "Direccion = @Direccion, Referencia = @Referencia, " +
+                    "IdTipoCliente = @IdTipoCliente, IdTipoDocumento = @IdTipoDocumento, " +
+                    "NumeroDocumento = @NumeroDocumento, Estado = @Estado " +
+                    "WHERE ID = @ID";
+                using(var comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.AddWithValue("@Nombres", cliente.Nombres);
+                    comando.Parameters.AddWithValue("@Marca", cliente.Marca);
+                    comando.Parameters.AddWithValue("@Precio", cliente.Precio);
+                    comando.Parameters.AddWithValue("@Stock", cliente.Stock);
+                    comando.Parameters.AddWithValue("@Estado", cliente.Estado);
+                    comando.Parameters.AddWithValue("@ID", cliente.ID);
+                    filasActualizadas = comando.ExecuteNonQuery();
+                }
+            }
+            return filasActualizadas > 0;
+        }
+
+        public bool Eliminar(int id)
+        {
+            int filasEiminadas = 0;
+            using(var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                var sql = "DELETE FROM Cliente WHERE ID = @id";
+                using(var comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.AddWithValue("@id", id);
+                    filasEiminadas = comando.ExecuteNonQuery();
+                }
+            }
+            return filasEiminadas > 0;
+        }
+    }
+}
