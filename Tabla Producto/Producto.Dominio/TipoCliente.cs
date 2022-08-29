e08dd07820dd38de536f3d331177fa4f75ab5502
Anthony Dimas Solano Torres <93417377+anthonyp20@users.noreply.github.com>
2022-08-29 10:52:40 -0500

    Delete Tabla Producto directory

--- Tabla Producto/Producto.Dominio/TipoCliente.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Financiera.Dominio
-{
-    public class TipoCliente
-    {
-        public int ID { get; set; }
-        public string Nombre { get; set; }
-        public bool Estado { get; set; }
-    }
-}
